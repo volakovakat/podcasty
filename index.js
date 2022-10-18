13c6c05b368af29b7681bbc98567f5573540aa00
Katerina Volakova <volakova.kat@gmail.com>
2022-10-18 20:40:49 +0200

    řešení

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-const podcast1 = {
-  num: '126',
-  title: 'Robot, který snědl koblihu',
-  guest: 'Radovan Siwek',
+import { Podcast } from "./Podcast/index.js";
+
+const episodes = [
+  {
+    num: 127,
+    title: 'Hledání plyšového Yettiho',
+    guest: 'Vojtěch Ryba',
+  },
+  {
+    num: 128,
+    title: 'Moderní hrachová polévka',
+    guest: 'Kamila Štancová',
+  },
+  {
+    num: 129,
+    title: 'Poloautomatické zrcadlo',
+    guest: 'Janka Janovská',
+  },
+  {
+    num: 130,
+    title: 'Máčené hlavy parlamentu',
+    guest: 'Jonáš Daněk',
+  },
+  {
+    num: 131,
+    title: 'Služby na hraně přívěsu',
+    guest: 'Tereza Křivánková',
+  },
+  {
+    num: 132,
+    title: 'Klasifikace sněžných klokanů',
+    guest: 'Josef Stix',
+  },
+  {
+    num: 133,
+    title: 'Rybolov v Oceánu bouří',
+    guest: 'Ludmila Gajová',
+  },
+];
+
+const renderPodcasts = () => {
+  document.querySelector(".episodes-list").innerHTML = episodes
+  .map((a) => Podcast(a))
+  .join('');
 };
 
-console.log('funguju!', podcast1);
+renderPodcasts();
